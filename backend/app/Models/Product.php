fd8d0f4de81ed2a853eaab2e04cb88b90f73dd26
Albert Ricart <contact@albertricart.dev>
2023-02-07 14:34:01 +0100

    Create Product laravel model

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $primaryKey = '_id';
+
+    /**
+     * Get the Customer that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customerId', 'customerId');
+    }
+}
