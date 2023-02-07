dce62500fa8ac8bd89c9323f4bcabde45f0074fe
Albert Ricart <contact@albertricart.dev>
2023-02-07 14:33:13 +0100

    Create customer table

---
+++ backend/database/migrations/2023_02_07_130139_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id('_id');
+            $table->string('nif');
+            $table->string('docNum');
+            $table->string('email');
+            $table->string('customerId')->unique();
+            $table->string('givenName');
+            $table->string('familyName1');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
