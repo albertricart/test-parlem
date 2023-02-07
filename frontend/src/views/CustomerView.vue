81fefb5e78fd722a675ae3f49d92e83205f8dfdd
Albert Ricart <contact@albertricart.dev>
2023-02-07 16:13:35 +0100

    Empty CustomerView

---
+++ frontend/src/views/CustomerView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log('CustomerView mounted.');
+});
+</script>
+
+<template>
+  <main>
+    <h1>Customer</h1>
+  </main>
+</template>
