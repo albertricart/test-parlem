c8fcb5c4c50dd66fef7958b153268cfd845f36fb
Albert Ricart <contact@albertricart.dev>
2023-02-07 14:33:50 +0100

    Create Customer laravel model

---
+++ backend/app/Models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customers';
+    protected $primaryKey = '_id';
+
+    /**
+     * Get all of the products for the Customer
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'customerId', 'customerId');
+    }
+}
